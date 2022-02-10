3ceec774ceb4529953019620e3b69e9fe2de83ee
Armin <arminparnian182@gmail.com>
2022-02-10 17:54:29 +0330

    58

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Components/person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    persons:[
+      {firstname:"Armin",lastname:"parnian",age:"22"}
+    ]
+  }
+change = ()=>{
+  this.setState({
+    persons:[
+      {firstname:"Pouya",lastname:"rajabi",age:"21"}
+    ]
+  })
+}
+  render(){
+    return (
+      <div className="App">
+        <h1>Armin Parnian</h1>
+        <hr />
+        <Person firstname={`${this.state.persons[0].firstname}`}
+         lastname={`${this.state.persons[0].lastname}`} 
+         age={`${this.state.persons[0].age}`}/>
+      <button className="btn" onClick={this.change}>change</button>
+      </div>
+    );
+  }
 }
 
 export default App;
